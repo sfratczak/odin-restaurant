ddb862eafac25059c55ecb706e94148511a1cbe5
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-07-17 21:07:01 +0200

    Add generating Home Page through script

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./reset.css";
 import "./style.css";
+import loadPageHome from "./page_home";
 
 const nav = document.createElement("nav");
 nav.textContent = "hello";
 const content = document.createElement("div");
-content.id = content;
+content.id = "content";
 
 document.body.appendChild(nav);
 document.body.appendChild(content);
+
+loadPageHome();
--- src/page_home.js
+++ src/page_home.js
@@ -3,7 +3,10 @@ import HomeImage from "./home.jpg";
 export default function loadPageHome() {
   const content = document.getElementById("content");
 
-  const img = document.createElement("img");
+  const contentMain = document.createElement("div");
+  contentMain.classList.add("content-main");
+
+  const img = new Image();
   img.src = HomeImage;
 
   const mainHeading = document.createElement("h1");
@@ -11,9 +14,9 @@ export default function loadPageHome() {
 
   const para = document.createElement("p");
   para.textContent =
-    "Immerse yourself in an ambiance reminiscent of a charming Parisian bistro as you savor every bite of our delectable French cuisine. Complemented by an extensive selection of fine wines, our menu offers a tantalizing journey through the culinary delights of France. <br /><br /> Join us for an unforgettable dining experience where the essence of French gastronomy comes to life.";
-
+    "Immerse yourself in an ambiance reminiscent of a charming Parisian bistro as you savor every bite of our delectable French cuisine. Complemented by an extensive selection of fine wines, our menu offers a tantalizing journey through the culinary delights of France. \r\n\r\nJoin us for an unforgettable dining experience where the essence of French gastronomy comes to life.";
   content.appendChild(img);
-  content.appendChild(mainHeading);
-  content.appendChild(para);
+  content.appendChild(contentMain);
+  contentMain.appendChild(mainHeading);
+  contentMain.appendChild(para);
 }
