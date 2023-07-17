1662434684c4f7a45844696682abca888b994ce8
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2023-07-17 21:43:00 +0200

    Add Nav Bar generation and page loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "./reset.css";
 import "./style.css";
+import loadNavBar from "./page_navbar";
 import loadPageHome from "./page_home";
 
 const nav = document.createElement("nav");
-nav.textContent = "hello";
 const content = document.createElement("div");
 content.id = "content";
 
 document.body.appendChild(nav);
 document.body.appendChild(content);
 
+loadNavBar();
 loadPageHome();
---
+++ src/page_navbar.js
@@ -0,0 +1,55 @@
+import loadPageHome from "./page_home";
+
+const navItems = {
+  //itemName: className
+  home: "nav-home",
+  menu: "nav-menu",
+  contact: "nav-contact",
+};
+
+function clearPage() {
+  const content = document.getElementById("content");
+
+  while (content.firstChild) {
+    content.removeChild(content.lastChild);
+  }
+}
+
+function loadPage(className) {
+  switch (className) {
+    case "nav-home":
+      loadPageHome();
+      break;
+    case "nav-menu":
+    // loadPageMenu();
+    // break;
+    case "nav-contact":
+      // loadPageContact();
+      // break;
+
+      return false;
+  }
+}
+
+export default function loadNavBar() {
+  const nav = document.querySelector("nav");
+  const navList = document.createElement("ul");
+
+  for (const [itemName, itemClassName] of Object.entries(navItems)) {
+    const navItem = document.createElement("li");
+
+    const navItemLink = document.createElement("a");
+    navItemLink.textContent = itemName;
+    navItemLink.classList.add(itemClassName);
+    navItemLink.href = "";
+    navItemLink.onclick = () => {
+      clearPage();
+      loadPage(itemClassName);
+    };
+
+    navItem.appendChild(navItemLink);
+    navList.appendChild(navItem);
+  }
+
+  nav.appendChild(navList);
+}
